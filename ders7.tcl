2f2dc388bd3c3f0e7a0a9fb56e97b794dc34713c
meyanik <93593481+meyanik@users.noreply.github.com>
2022-03-09 07:45:04 +0300

    Add files via upload

---
+++ ders7.tcl
@@ -0,0 +1,19 @@
+
+;# expr komutunun kullanimi
+
+set X 100;
+set Y 256;
+set Z [expr "$Y + $X"]
+set Z_LABEL "$Y + $X = "
+
+puts "$Z_LABEL $Z"
+puts "$Y degerinin karekoku = [expr sqrt($Y)]\n"
+
+puts "oncelik kurallarina bagli olarak \"5 + -3 * 4\"   =: [expr -3 * 4 + 5]"
+puts "Because of the parentheses      \"(5 + -3) * 4\"  =: [expr (5 + -3) * 4]"
+
+puts "\n.................\n \" ve \{ uzerine birkac ornege daha bakalim\n.................\n"
+puts {$Z_LABEL [expr $Y + $X]}
+puts "$Z_LABEL {[expr $Y + $X]}"
+puts "iki degiskeni toplamak icin kullanacagimiz komut: \[expr \$a + \$b]"
+
