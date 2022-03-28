e01f1a11adfd0b169e001a8438e0edfce2467dd7
meyanik <93593481+meyanik@users.noreply.github.com>
2022-03-28 14:53:47 +0300

    Add files via upload

---
+++ ders13.tcl
@@ -0,0 +1,25 @@
+
+
+;# Bu ornekte bir degiskeni fonksiyon kulanarak set etmeyi gorecegiz
+
+proc fonksiyonum {degisken1 {degisken2 ""} {args ""}} {
+  if {$degisken2 == ""} {
+    puts "fonksiyona tek bir degisken verildi: $degisken1";
+    return 1;
+    } else {
+    if {$args == ""} {
+      puts "Fonksiyona iki degisken verildi- $degisken1 ve $degisken2";
+      return 2;
+      } else {
+      puts "Fonksiyona uc degisken verildi - $degisken1 ve $degisken2 ve $args";
+      return "daha fazla";
+      }
+    }
+  }
+
+set count1 [fonksiyonum bir]
+set count2 [fonksiyonum bir iki]
+set count3 [fonksiyonum bir iki uc ]
+set count4 [fonksiyonum bir iki uc dort]
+
+puts "Bu fonksiyon $count1, $count2, $count3, ve $count4 degiskenler ile cagirildi"
